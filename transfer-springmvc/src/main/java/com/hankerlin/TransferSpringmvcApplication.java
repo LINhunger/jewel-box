ae18d12c6a53cbf04171b526bd20658843b3ad9f
LINhunger <466400960@qq.com>
2018-10-28 22:25:18 +0800

    201810281

---
+++ transfer-springmvc/src/main/java/com/hankerlin/TransferSpringmvcApplication.java
@@ -0,0 +1,12 @@
+package com.hankerlin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferSpringmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferSpringmvcApplication.class, args);
+	}
+}
